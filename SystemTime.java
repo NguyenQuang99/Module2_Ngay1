ccd44c4da10951bb2ab9aa1cd4e7f0da1e8e20b0
NguyenQuang99 <quanghero7@gmail.com>
2020-09-09 11:59:06 +0700

    SystemTime.java

---
+++ SystemTime.java
@@ -0,0 +1,10 @@
+package Thuchanh_Hienthithoigian;
+
+import java.util.Date;
+
+public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Now is " + now);
+    }
+}
